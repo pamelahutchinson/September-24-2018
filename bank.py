e8c920b4409dbd4992dacd9d71ef92ac08beaa20
Pamela Hutchinson <pamelahutchinson@Pamelas-MBP.localdomain>
2018-09-28 11:27:08 -0500

    updated todolist.py to add json-updated the bank with withdraw option

--- bank.py
+++ bank.py
@@ -16,8 +16,21 @@
 # close():
 #     isOpen = false
 
+# class User:
+#     def __init__(self, first_name,last_name,middle_name):
+#         self.bank_accounts = []
+
+#         user = User(.....,...,...)-->pass this in the BankAccount class
+
+
 #-----------------------------
 funds = []
+#accounts = []
+users = []
+balance = []
+status_of_account = ["OPEN","CLOSED","FREEZE"]
+user_id_count = 1
+account_id_count = 1
 
 def print_menu():
     print("Welcome to Pam's Bank!")
@@ -27,63 +40,139 @@ def print_menu():
     print("4.Fees and regulations")
     print("5.EXIT")
 
-class BankAccount:
-    def __init__(self,first_name,last_name,middle_name,balance,):
+class User:
+    def __init__(self,first_name,last_name,middle_name):
         self.first_name = first_name
         self.last_name = last_name
         self.middle_name = middle_name
-        #self.account_type = account_type
+        self.accounts = []
+        self.id = user_id_count
+    def __repr__(self):
+        return  '%s %s %s - %s' % (self.first_name, self.middle_name, self.last_name, self.accounts)
+
+class BankAccount:
+    def __init__(self,account_type,balance,status):
+        self.account_type = account_type
         self.balance = balance
-        #self.status = status
+        self.status = status
+        self.account_id = account_id_count
+        #self.total = 0
+    def __repr__(self):
+        return  '%s %s, %s' % (self.account_type, self.balance, self.status)
 
-def withdraw(self):
-    print("Current balance:" ,self.balance)
-    with_amount = int(input("How much would you like to withdraw: "))
-    if (with_amount < self.balance):
-        self.balance -= with_amount
-        print("New balance = ",self.balance)
-        print("******************************************")
+def withdraw(account_selection,with_amount):
+    new_balance = account_selection.balance - with_amount
+    if new_balance < 0:
+        new_balance = account_selection.balance - 35
+        print("Insufficient funds!!-$35.00 overdraft fee applied.")
+        print(f"Your new balance is {new_balance}")
+        return new_balance
     else:
-        print("Insufficient funds!!! Please try again.")
-        withdraw(b1)
+        print(f"Your new balance is {new_balance}")
+        return new_balance
+
+    # account_selection = input("Please select the account: ")
+    # print(self.accounts)
+    # if (account_selection =="1" "2","3"):
+    #     return self.accounts[account_selection]
+    # print("Current balance:" ,self.accounts.balance)
+    # with_amount = int(input("How much would you like to withdraw: "))
+    # if (with_amount < self.balance):
+    #     self.balance -= with_amount
+    #     print("New balance:",self.balance)
+    #     print("******************************************")
+
+    # else:
+    #     self.balance -= with_amount
+    #     self.balance -= 35
+    #     print("Insufficient funds!!-$35.00 overdraft fee applied. Refer to fees and regulations for more info.")
+    #     print("New balance: ",self.balance)
+    #     self.status = status_of_account[2] 
+
+def open_account():
+    account_type = input("Enter type of account: ")
+        
+    balance = int(input("Enter current balance: "))
+    if(balance < 100):
+        print("Unable to open bank account. Please refer to -Fees and Regulations- on the main menu for more info!")
+        pass
+    else:
+        global user
+        status = status_of_account[0]
+        bank_account = BankAccount(account_type,balance,status)
+        user.accounts.append(bank_account)
+        print(user)
+        new_account_open()
+
+def new_account_open():
+    new_account = input("Enter (y) if you would like to open another account or press (any key) to exit: ")
+    if new_account == "y":
+        open_account()
+
+    else:
+        pass
+        
 
 
+#bank_account = BankAccount("Savings",5000,"OPEN")
+#user = User("Obi","Ezeakachi","Oko")
+#user.accounts.append(bank_account)
 
-
-b1 = BankAccount("Obi","Ezeakachi","Oko",5000)
-b2 = BankAccount("Tasha","Tomas","KI",8000)
-b3 = BankAccount("Obi","Ezeakachi","Oko",7000)
+#b2 = BankAccount("Tasha","Tomas","KI",8000)
+#b3 = BankAccount("Obi","Ezeakachi","Oko",7000)
 
 
 loop = True
 while loop:
     print_menu()
-    option = int(input("Please select an option [1-5]: "))
-    
-    if (option == 1):
+    option = (input("Please select an option [1-5]: "))
+    if (option == "1"):
         print("*Option 1 has been selected*")
         first_name = input("Enter first name: ")
         middle_name = input("Enter middle name: ")
         last_name = input("Enter last name: ")
+        account_type = input("Enter type of account: ")
         
-        minumun_balance = int(input("Enter current balance: "))
-        if(minumun_balance < 100):
+        balance = int(input("Enter current balance: "))
+        if(balance < 100):
             print("Unable to open bank account. Please refer to -Fees and Regulations- on the main menu for more info!")
-            continue
-
-    elif (option ==2):
+            pass
+        else:
+            status = status_of_account[0]
+            user = User(first_name,middle_name,last_name)
+            user_id_count += 1
+            users.append(user)
+            bank_account = BankAccount(account_type,balance,status)
+            account_id_count += 1
+            user.accounts.append(bank_account)
+            print(user)
+        new_account_open() 
+       
+        
+        
+    elif (option == "2"):
         print("*Option 2 has been selected*")
-    elif(option == 3):
+
+    elif(option == "3"):
         print("*Option 3 has been selected*")
-        withdraw(b1)  
-    elif(option == 4):
+        user_id = int(input("Enter the User ID that owns the account: "))
+        for user in users:
+            if user.id == user_id:
+                list_of_accounts = user.accounts
+        account_to_withdraw = int(input("What is the ID of the account? "))
+        for account in list_of_accounts:
+            if account.account_id == account_to_withdraw:
+                chosen_account = account
+        amount_to_withdraw = int(input("How much would you like to withdraw? $"))
+        withdraw(chosen_account, amount_to_withdraw)  
+    elif(option == "4"):
         print("*Option 4 has been selected*")
         print("To open a bank account --> Initial balance has to be minumum $100.00")
         print("If bank account balance is lower than $0.00 --> Account will be charged an ovedraft fee of $35.00")
         question4 = input("Please press (b) to go back to the main menu: ")
         if (question4 == "b"):
             continue
-    elif(option == 5):
+    elif(option == "5"):
         print("*Option 5 has been selected*")
         print("Thank you for using our bank app!")
         loop = False
--- todolist.py
+++ todolist.py
@@ -1,70 +1,86 @@
 
+import json
+
 tasks = []
 count = 0
 
 
-#************************************
+#***********************************
 class Task:
     def __init__(self,title,priority):
-
         global count
-
-        self.title = title
-        self.priority = priority
         count += 1
         self.taskid = count
+        self.title = title
+        self.priority = priority
+       
 
+    def asDictionary(self):
+        return self.__dict__
 
+def print_menu():
+    print("****TO DO LIST****")
+    print("-------Menu-------")
+    print("1.Add New Task")
+    print("2.Delete Task")
+    print("3.Quit")
 
 def delete_task_by_id(task_id):
-
     index_to_delete = -1 
-
     for index in range(0,len(tasks)):
         if(tasks[index].taskid == task_id):
             index_to_delete = index 
             break 
-    
     return index_to_delete
 
 
 
-
-while True:
-
-    title = input("Enter task title or q to quit: ")
-    if (title == "q"):
-        break
-
-    priority = input("Enter HIGH/MEDIUM/LOW priority: ")
-    
-    task = Task(title,priority)
-    tasks.append(task)
-    
-
-    view_add = input("Press (v) to view Task list or press (any key) to add another task: ")
-
-    if (view_add == "v"):
-        for task in tasks:
-            #sorted(tasks, key=lambda task:task.priority)
-            print(task.taskid,".",task.title,"-",task.priority)
-
-        delete_task = input("Press (d) to delete a specific task, press (any key) to add another task or press (q) to quit app: ")   
-        
-        if (delete_task == "d"):
-            which_task = int(input("Please select task number: "))
-
-            index_to_delete = delete_task_by_id(which_task)
-            if(index_to_delete >= 0):
-                del tasks[index_to_delete]
-
-            #del tasks[which_task - 1]
+loop = True
+while loop:
+    print_menu()
+    try:
+        option = int(input("Select option [1-3]: "))
+        if (option == 1):
+            title = input("Enter task title: ")
+            priority = input("Enter HIGH/MEDIUM/LOW priority: ")
+            task = Task(title,priority)
+            tasks.append(task.asDictionary())
+            view_add = input("Press (v) to view Task list or press (any key) to go back to Main Menu: ")
+            if (view_add == "v"):
+                for task in tasks:
+                    print(task.taskid,".",task.title,"-",task.priority)
+            else:
+                continue
+        elif (option == 2):
             for task in tasks:
                 print(task.taskid,".",task.title,"-",task.priority)
-                
-        elif(delete_task == "q"):
-            break
-        else:
-            continue
-    
-print("Thank you for using the App!!")
+            delete_task = input("Press (d) to delete a specific task, press (any key) to go back to the Main Menu: ")  
+            if (delete_task == "d"):
+                try:
+                    which_task = int(input("Please select task number: "))
+                    index_to_delete = delete_task_by_id(which_task)
+                    if(index_to_delete >= 0):
+                        del tasks[index_to_delete]
+                        for task in tasks:
+                            print(task.taskid,".",task.title,"-",task.priority)
+                except ValueError:
+                    print("Invalid entry.Please select numerical number from the task!")
+                    
+            else:
+                continue
+        elif(option == 3):
+            print("You have existed the app.")
+            print("Thank you for using the App!!")
+            loop = False
+
+    except ValueError:
+        print("Invalid numerical entry. Please try again!")
+        
+
+with open("to_do_list.json","w") as file_object:
+    json.dump(tasks,file_object,indent=2)
+
+
+with open("to_do_list.json","r") as file_object:
+    task_data = json.load(file_object)
+    print(task_data)
